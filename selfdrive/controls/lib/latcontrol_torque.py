3cbd809e0f92824f1c73fc4a0759063c06d1da5e
ClockeNessMnstr <locke.dftc@gmail.com>
2022-06-07 12:41:03 -0400

    update DH names + notes for MPC output curvatures (#24701)
    * update names + notes for MPC outputs
    
    "current_curvature" is not the correct description of what the MPC is outputting in it's curvature_ego state.
    The MPC is integrating it's free variable, curvature_rate, such that curvature[0] is actually the desired_curvature before any delay.
    inversely: the curvature_rate_desired is the desired rate of change to the setpoint and not the actual curvature rate. 
    
    If we were to set the initial curvature = measured curvature in the MPC initiation these names would be correct. 
    This was possibly how it was initially set up but the nomenclature here is now confusing.
    
    * more notes
    
    * match
    
    * Clarify #1

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -60,9 +60,9 @@ class LatControlTorque(LatControl):
         actual_curvature = interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_llk])
         curvature_deadzone = 0.0
       desired_lateral_accel = desired_curvature * CS.vEgo ** 2
-
+      
       # desired rate is the desired rate of change in the setpoint, not the absolute desired curvature
-      #desired_lateral_jerk = desired_curvature_rate * CS.vEgo ** 2
+      desired_lateral_jerk = desired_curvature_rate * CS.vEgo ** 2
       actual_lateral_accel = actual_curvature * CS.vEgo ** 2
       lateral_accel_deadzone = curvature_deadzone * CS.vEgo ** 2
 
