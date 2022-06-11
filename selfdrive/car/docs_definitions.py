02559e6f13af359336e6944767b5bc0d47649750
HaraldSchafer <harald.the.engineer@gmail.com>
2022-06-10 17:57:23 -0700

    Car interface: set max lateral torque from table (#24789)
    * json
    
    * better naem
    
    * Read from table
    
    * formatting and default to nan
    
    * Generate docs
    
    * Read from table
    
    * this should be the same
    
    * Prius v is full
    
    * test we always set the tunes correctly
    
    add to release files
    
    * Set for all cars
    
    Set for all cars
    
    * Revert tuning changes
    
    Revert tuning changes
    
    * remove that
    
    * fixes
    
    * update ref commit for new maxLateralAccels
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- selfdrive/car/docs_definitions.py
+++ selfdrive/car/docs_definitions.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List, Optional, Union, no_type_check
 
+TACO_TORQUE_THRESHOLD = 2.5  # m/s^2
+GREAT_TORQUE_THRESHOLD = 1.4  # m/s^2
+GOOD_TORQUE_THRESHOLD = 1.0  # m/s^2
+
 
 class Tier(Enum):
   GOLD = "The best openpilot experience. Great highway driving and beyond."
