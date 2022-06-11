3b75266a34c156d9b98529af32f8777214ce21ad
HaraldSchafer <harald.the.engineer@gmail.com>
2022-06-10 22:52:48 -0700

    Misc torque control fixes (#24801)
    * Fiction compensation should be based on error
    
    * Update refs
    
    * Add deadzone
    
    * update ref
    
    (cherry picked from commit 843e59f6f0be33e6c51a35ef8912f5b7c4b58ca5)

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -38,8 +38,8 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.74   # unknown end-to-end spec
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.INDI_PRIUS)
-      ret.steerActuatorDelay = 0.3
+      ret.maxLateralAccel = 1.7
+      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=ret.maxLateralAccel, FRICTION=0.06, steering_angle_deadzone_deg=1.0)
 
     elif candidate == CAR.PRIUS_V:
       stop_and_go = True
