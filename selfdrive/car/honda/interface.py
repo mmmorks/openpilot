f576e51f1d4377d85e155cf1f15793c8044711d5
John Morkel <john@morkel.me>
2022-06-23 09:28:19 -0700

    Add back missing import

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -7,6 +7,7 @@ from selfdrive.car.honda.values import CarControllerParams, CruiseButtons, Honda
 from selfdrive.car import STD_CARGO_KG, CivicParams, create_button_enable_events, create_button_event, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
 from selfdrive.car.disable_ecu import disable_ecu
+from selfdrive.controls.lib.latcontrol_torque import set_torque_tune
 
 
 ButtonType = car.CarState.ButtonEvent.Type
