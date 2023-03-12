3e24fa086a90b590e86dffa256ca1118353e2c2e
John Morkel <john@morkel.me>
2023-03-11 23:34:49 -0800

    Added testing and fixed many issues

--- eps-update.py
+++ eps-update.py
@@ -2,9 +2,12 @@ import gzip
 import os
 import struct
 import sys
+import tqdm
+import traceback
 from panda.format.x5a import x5a
 from panda import Panda
-from panda.python.uds import UdsClient, SESSION_TYPE, ACCESS_TYPE
+from panda.python.uds import UdsClient, SESSION_TYPE, ACCESS_TYPE, ROUTINE_CONTROL_TYPE, ROUTINE_IDENTIFIER_TYPE, DATA_IDENTIFIER_TYPE
+from unittest import mock
 
 def read_file(fn):
     f_name, f_ext = os.path.splitext(fn)
@@ -19,7 +22,9 @@ def read_file(fn):
 
     return f_data
 
-def validate_fw(fw, block_end_addrs):
+def validate_fw(fw_encrypted, cipher_ops, block_end_addrs):
+    assert len(fw_encrypted.firmware_blocks) == 1 # don't know how to handle firmware with more than one section
+    fw = decrypt(fw_encrypted, cipher_ops)
     sum = 0 # sum at each checksum should be 0 so we don't need to reset it
     start = 0
     end = max(block_end_addrs)
@@ -55,73 +60,125 @@ def decrypt(fw, ops):
     plain, _ = fw.decrypt(decoder)
     return plain
 
+def get_uds_client(can_addr):
+    try:
+        panda = Panda(disable_checks=True)
+        panda.set_safety_mode(Panda.SAFETY_ELM327)
+        uds_client = UdsClient(panda, can_addr, debug=False)
+        print("Using real client")
+    except:
+        mock_helper = mock.patch('panda.python.uds.UdsClient', autospec=True)
+        uds_client = mock_helper.start()
+        uds_client.security_access.return_value = b'1234'
+        uds_client.request_download.return_value = 514
+        uds_client.read_data_by_identifier.return_value = b'39990-TG7-A030\x00\x00'
+        print("Using mock client")
+
+    return uds_client
+
+def get_seed_secret(fw, app_id):
+    headers = fw.file_headers
+    for i in range(len(headers[4].values)):
+        if headers[3].values[i].value == app_id:
+            return headers[4].values[i].value
+
+    raise Exception("Couldn't find software seed for software application ID {}".format(app_id))
+
+def get_can_address(fw):
+    return 0x18da00f1 | struct.unpack('!B', fw.file_headers[2].values[0].value)[0] << 8
+
 if __name__ == "__main__":
   f_name = sys.argv[1] #"\\\\wsl$\\Ubuntu\\home\\john\\Code\\greg-rwd-xray\\39990-TG7-A060-M1.rwd.gz" #sys.argv[1]
-  fw_ops = '+^-' #sys.argv[2]
+  cipher_ops = '+^-' #sys.argv[2]
   f_raw = read_file(f_name)
   fw = x5a(f_raw)
 
-  validate_fw(decrypt(fw, fw_ops), [0xa000, 0x1d000, 0x4ff00])
-  assert len(fw.firmware_blocks) == 1
+  validate_fw(fw, cipher_ops, [0xa000, 0x1d000, 0x4ff00])
 
   print(fw)
 
-  panda = Panda()
-  panda.set_safety_mode(Panda.SAFETY_ELM327)
-  address = 0x18da30f1 # EPS
-  uds_client = UdsClient(panda, address, debug=False)
+  can_addr = get_can_address(fw)
+  print("Connecting to CAN address 0x{:08X}".format(can_addr))
+  uds_client = get_uds_client(can_addr)
+
   print("tester present ...")
   uds_client.tester_present()
 
   try:
+    print("Getting software version")
+    app_id = uds_client.read_data_by_identifier(DATA_IDENTIFIER_TYPE.APPLICATION_SOFTWARE_IDENTIFICATION)
+    print("Application Software ID = {}".format(app_id))
+    
     print("Set diagnostic session type to 3 (extended diagnostic)")
     data = uds_client.diagnostic_session_control(SESSION_TYPE.EXTENDED_DIAGNOSTIC)
-    print(data)
     
     print("Security access request key for seed 1")
     data = uds_client.security_access(ACCESS_TYPE.REQUEST_SEED)
-    print(data)
-    key = calculate_session_key(data[-2:])
+    secret_key = get_seed_secret(fw, app_id)
+    key = calculate_session_key(secret_key, data[-2:])
     print("key = ", key)
 
     print("Security access send key for seed 1")
     data = uds_client.security_access(ACCESS_TYPE.SEND_KEY, key)
-    print(data)
 
     print("Set diagnostic session type to programming")
     data = uds_client.diagnostic_session_control(SESSION_TYPE.PROGRAMMING)
-    print(data)
 
     print("Erasing flash")
     data = uds_client.routine_control(ROUTINE_CONTROL_TYPE.START, ROUTINE_IDENTIFIER_TYPE.ERASE_MEMORY)
-    print(data)
 
     print("Setting firmware decryption key")
     data = uds_client.write_data_by_identifier(DATA_IDENTIFIER_TYPE.FLASH_DECRYPTION_KEY, fw.keys)
-    print(data)
 
     print("Requesting download")
+    assert len(fw.firmware_blocks) == 1
     block = fw.firmware_blocks[0]
     length = block["length"]
     max_chunk_size = uds_client.request_download(block["start"], length)
     max_chunk_size -= 2 # subtract header bytes
 
-    cursor = 0x0
-    seq = 0
-    while cursor < length:
-        block_size = min(max_chunk_size, length - cursor) 
-        data = uds_client.transfer_data(seq, fw.firmware_encrypted[0][cursor:cursor+block_size])
-        print(data)
-        seq += 1
+    with tqdm.tqdm(total=length, unit='B', unit_scale=True) as t:
+        cursor = 0x0
+        seq = 0
+        while cursor < length:
+            block_size = min(max_chunk_size, length - cursor) 
+            data = uds_client.transfer_data(seq, fw.firmware_encrypted[0][cursor:cursor+block_size])
+            seq += 1
+            cursor += block_size
+            t.update(block_size)
     
     print("Requesting transfer exit") 
     data = uds_client.request_transfer_exit()
-    print(data)
 
     print("Checking programming dependencies")
     data = uds_client.routine_control(ROUTINE_CONTROL_TYPE.START, ROUTINE_IDENTIFIER_TYPE.CHECK_PROGRAMMING_DEPENDENCIES)
-    print(data)
 
-  except BaseException as e:
-    print(e)
+  except Exception:
+    print(traceback.format_exc())
 
+if isinstance(uds_client, mock.Mock):  
+    from unittest.mock import ANY, call
+    
+    #print(uds_client.method_calls)
+
+    calls = []
+    calls += [call.read_data_by_identifier(DATA_IDENTIFIER_TYPE.APPLICATION_SOFTWARE_IDENTIFICATION)]
+    calls += [call.diagnostic_session_control(SESSION_TYPE.EXTENDED_DIAGNOSTIC)]
+    calls += [call.security_access(ACCESS_TYPE.REQUEST_SEED)]
+    calls += [call.security_access(ACCESS_TYPE.SEND_KEY, ANY)]
+    calls += [call.diagnostic_session_control(SESSION_TYPE.PROGRAMMING)]
+    calls += [call.routine_control(ROUTINE_CONTROL_TYPE.START, ROUTINE_IDENTIFIER_TYPE.ERASE_MEMORY)]
+    calls += [call.write_data_by_identifier(DATA_IDENTIFIER_TYPE.FLASH_DECRYPTION_KEY, fw.keys)]
+    calls += [call.request_download(0x10000, 0x50000)]
+    calls += [call.transfer_data(0, fw.firmware_encrypted[0][0:512])]
+    calls += [call.transfer_data(1, fw.firmware_encrypted[0][512:1024])]
+    uds_client.assert_has_calls(calls)
+
+    num_blocks = -(len(fw.firmware_encrypted[0]) // -512) # sneaky math ceil
+    assert uds_client.transfer_data.call_count == num_blocks
+
+    calls = []
+    calls += [call.transfer_data(num_blocks - 1, fw.firmware_encrypted[0][((num_blocks-1)*512):])]
+    calls += [call.request_transfer_exit()]
+    calls += [call.routine_control(ROUTINE_CONTROL_TYPE.START, ROUTINE_IDENTIFIER_TYPE.CHECK_PROGRAMMING_DEPENDENCIES)]
+    uds_client.assert_has_calls(calls)
